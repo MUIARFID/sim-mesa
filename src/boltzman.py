becaf11f388e9896dace0afc14908158c141d643
edoelas <miego.cvcv@gmail.com>
2024-02-16 11:18:40 +0000

    asd

--- src/boltzman.py
+++ src/boltzman.py
@@ -3,9 +3,15 @@ import seaborn as sns
 import numpy as np
 import pandas as pd
 
-class MoneyAgent(mesa.Agent):
-    """An agent with fixed initial wealth."""
+def compute_gini(model):
+    agent_wealths = [agent.wealth for agent in model.schedule.agents]
+    x = sorted(agent_wealths)
+    N = model.num_agents
+    B = sum(xi * (N - i) for i, xi in enumerate(x)) / (N * sum(x))
+    return 1 + (1 / N) - 2 * B
 
+
+class MoneyAgent(mesa.Agent):
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
         self.wealth = 1
@@ -18,34 +24,39 @@ class MoneyAgent(mesa.Agent):
         self.model.grid.move_agent(self, new_position)
 
     def give_money(self):
+        if self.wealth == 0: return # No hay riqueza para dar
         cellmates = self.model.grid.get_cell_list_contents([self.pos])
-        if len(cellmates) > 1:
-            other_agent = self.random.choice(cellmates)
-            other_agent.wealth += 1
-            self.wealth -= 1
+        cellmates.pop(cellmates.index(self)) # No se puede dar a uno mismo
+        if not cellmates: return # No hay vecinos
+        other = self.random.choice(cellmates)
+        other.wealth += 1
+        self.wealth -= 1
 
     def step(self):
         self.move()
-        if self.wealth > 0:
-            self.give_money()
+        self.give_money()
 
 
 class MoneyModel(mesa.Model):
-    """A model with some number of agents."""
-
     def __init__(self, N, width, height):
         super().__init__()
         self.num_agents = N
         self.grid = mesa.space.MultiGrid(width, height, True)
         self.schedule = mesa.time.RandomActivation(self)
+
         # Create agents
         for i in range(self.num_agents):
-            a = MoneyAgent(i, self)
-            self.schedule.add(a)
+            agent = MoneyAgent(i, self)
+            self.schedule.add(agent)
             # Add the agent to a random grid cell
             x = self.random.randrange(self.grid.width)
             y = self.random.randrange(self.grid.height)
-            self.grid.place_agent(a, (x, y))
+            self.grid.place_agent(agent, (x, y))
+
+        self.datacollector = mesa.DataCollector(
+            model_reporters={"Gini": compute_gini}, agent_reporters={"Wealth": "wealth"}
+        )
 
     def step(self):
+        self.datacollector.collect(self)
         self.schedule.step()
